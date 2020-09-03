3ad72803aa397d70b0430b01ab760838ee734658
amckean12 <amckean@me.com>
2020-09-02 19:22:24 -0500

    adds header

--- simulation-manager/src/App.js
+++ simulation-manager/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './header.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      "Hello World"
+      <Header />
     </div>
   );
 }
---
+++ simulation-manager/src/header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render(){
+        return(
+            <div className="container-fluid p-0">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">Wood.</a>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link 2</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link 3</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link 4</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Header;
