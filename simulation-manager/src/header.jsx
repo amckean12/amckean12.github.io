6b493b9569de5efbc59df56889e88148c4273662
amckean12 <amckean@me.com>
2020-09-02 19:44:04 -0500

    header defaults:

--- simulation-manager/src/header.jsx
+++ simulation-manager/src/header.jsx
@@ -8,19 +8,19 @@ class Header extends React.Component {
                     <a class="navbar-brand" href="#">Wood.</a>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
-                            <li class="nav-item">
+                            <li class="nav-item px-4">
                                 <a class="nav-link" href="#">Link</a>
                             </li>
-                            <li class="nav-item">
+                            <li class="nav-item px-4">
                                 <a class="nav-link" href="#">Link 2</a>
                             </li>
-                            <li class="nav-item">
+                            <li class="nav-item px-4">
                                 <a class="nav-link" href="#">Link 3</a>
                             </li>
-                            <li class="nav-item">
+                            <li class="nav-item px-4">
                                 <a class="nav-link" href="#">Link 4</a>
                             </li>
-                            <li class="nav-item">
+                            <li class="nav-item flex-right">
                                 <a class="nav-link disabled" href="#">Disabled</a>
                             </li>
                         </ul>
