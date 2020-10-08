6a7ba00f270cf3a297b5058f7511256734fccdae
amckean12 <amckean@me.com>
2020-10-08 17:02:49 -0500

    Adds container function

--- simulation-manager/src/containers/simulation-scheduler.jsx
+++ simulation-manager/src/containers/simulation-scheduler.jsx
@@ -4,8 +4,27 @@ import React from 'react';
 class SimulationSchedulerContainer extends React.Component {
     render(){
         return(
-            <main className="simulation-scheduler-container">
-                Simulation Scheduler Container
+            <main className="container-fluid p-0 simulation-scheduler-container">
+                <section className="row pt-5 border-bottom">
+                    <div className="col-md-6">
+                        <div className="dropdown d-flex justify-content-center">
+                            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Simulation Type</button>
+                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a className="dropdown-item" href="#">Multi Stage Flow</a>
+                                <a className="dropdown-item" href="#">SWMM</a>
+                            </div>
+                        </div>
+                        <button type="button" class="btn btn-outline-secondary">Secondary</button>
+                    </div>
+                    <div className="col-md-6 d-flex flex-column align-items-center">
+                        <div className="button-wrapper w-50">
+                            <button type="button" class="btn btn-outline-secondary">Select Model</button>
+                        </div>
+                        <div className="button-wrapper w-50">
+                            <button type="button" class="btn btn-outline-secondary">Select Model</button>
+                        </div>
+                    </div>
+                </section>
             </main>
         )
     }
--- simulation-manager/src/index.js
+++ simulation-manager/src/index.js
@@ -5,7 +5,11 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/index.js';
-
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
+import Popper from 'popper.js';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
